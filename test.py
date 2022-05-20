3ae044e52ff54c01179ba2236053f0357f16d9a3
2jinu <ejinu2011@gmail.com>
2022-05-20 20:27:01 +0900

    22.05.20

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
-import os, tqdm
-import numpy as np
-from PIL import Image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
+import pandas
+from sqlalchemy import inspect, create_engine
 
-base_dir        = os.path.join(os.getcwd(),'python')
-image_dir       = os.path.join(base_dir, 'apple')
-output_dir      = os.path.join(base_dir, 'dataset')
-width, height   = 32, 32
-dataset         = []
-datagen         = ImageDataGenerator(rotation_range = 20, # 이미지 회전
-                                    zoom_range = [0.5, 1.0],
-                                    width_shift_range=0.1, #이미지 좌우로 움직이기
-                                    height_shift_range=0.1, #이미지 위아래로 움직이기
-                                    fill_mode='nearest' #빈값 채우기
-                                    )
 
-for img_file in tqdm.tqdm(os.listdir(image_dir)):
-    img             = img_to_array(Image.open(os.path.join(image_dir, img_file)).resize((width, height)).convert('RGB'))
-    expanded_img    = np.expand_dims(img, 0)
-    datagen.fit(expanded_img)
-    i   = 0
-    for x in datagen.flow(expanded_img, save_to_dir=output_dir, save_prefix=1, save_format='jpg'):
-        i+=1
-        if i == 20: break
+dbms        = 'mysql'
+database    = 'test'
+user        = 'root'
+password    = 'root'
+host        = '127.0.0.1'
+port        = 3306
+engine      = create_engine(f'{dbms}://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4')
+inspector   = inspect(engine)
+
+table_names = inspector.get_table_names(database) # 데이터베이스의 테이블 명 리스트 반환
+column_names= [column if inspector else None for column in inspector.get_columns(table_names[0], schema=database)] # 테이블의 컬럼 이름 리스트 반환
+
+df.to_sql(name=table_names[0], con=engine, if_exists='append', index=False) # 테이블에 데이터 추가
+pandas.read_sql('SELECT * FROM {}'.format(table_names[0]), con=engine) # 테이블의 데이터 읽기
